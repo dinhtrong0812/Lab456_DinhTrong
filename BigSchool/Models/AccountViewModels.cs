6e38aa9baad36f2c43d463fa08e6aacc59b0cd2b
trong nguyen <dinhtrong0812@gmail.com>
2020-06-15 08:36:02 +0700

    ert

--- BigSchool/Models/AccountViewModels.cs
+++ BigSchool/Models/AccountViewModels.cs
@@ -8,6 +8,7 @@ namespace BigSchool.Models
         [Required]
         [Display(Name = "Email")]
         public string Email { get; set; }
+
     }
 
     public class ExternalLoginListViewModel     
